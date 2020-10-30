3c5c6e7d3c425ff46d1b7b5971f8d55a0d06f6cc
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-30 23:45:06 +0530

    Added FormsActivity and FormViewActivity

---
+++ app/src/main/java/amcoders/tamiltech/FormViewActivity.java
@@ -0,0 +1,14 @@
+package amcoders.tamiltech;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_view);
+    }
+}
---
+++ app/src/main/java/amcoders/tamiltech/FormsActivity.java
@@ -0,0 +1,25 @@
+package amcoders.tamiltech;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormsActivity extends AppCompatActivity {
+    private LinearLayout formLLT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forms);
+        formLLT = (LinearLayout) findViewById(R.id.form_item_llt);
+
+        formLLT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
