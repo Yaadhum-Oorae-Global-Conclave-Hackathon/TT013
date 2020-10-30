53261859fa204349b1cff8211933fd6919531fec
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-30 15:49:23 +0530

    Fixed the bugs

--- app/src/main/java/amcoders/tamiltech/LoginActivity.java
+++ app/src/main/java/amcoders/tamiltech/LoginActivity.java
@@ -1,15 +1,29 @@
 package amcoders.tamiltech;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView text_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        text_view = findViewById(R.id.text_view);
+
+        text_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
--- app/src/main/java/amcoders/tamiltech/MainActivity.java
+++ app/src/main/java/amcoders/tamiltech/MainActivity.java
@@ -1,29 +1,17 @@
 package amcoders.tamiltech;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text_view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
 
-        text_view = findViewById(R.id.text_view);
-
-        text_view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-            }
-        });
     }
 }
