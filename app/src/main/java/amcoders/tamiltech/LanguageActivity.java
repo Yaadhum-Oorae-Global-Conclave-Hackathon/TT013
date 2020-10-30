8022b7b1366522838d81e737d1ca451054cc4d76
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-30 22:06:04 +0530

    Removed hindi

--- app/src/main/java/amcoders/tamiltech/LanguageActivity.java
+++ app/src/main/java/amcoders/tamiltech/LanguageActivity.java
@@ -1,17 +1,17 @@
 package amcoders.tamiltech;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class LanguageActivity extends AppCompatActivity {
 
-    private TextView EngSelect, TamSelect, HindiSelect;
+    private TextView EngSelect, TamSelect;
     private String language;
 
 
@@ -22,7 +22,6 @@ public class LanguageActivity extends AppCompatActivity {
 
         EngSelect = (TextView) findViewById(R.id.eng_select);
         TamSelect = (TextView) findViewById(R.id.tamil_select);
-        HindiSelect = (TextView) findViewById(R.id.hindi_select);
 
         EngSelect.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -32,8 +31,6 @@ public class LanguageActivity extends AppCompatActivity {
                 EngSelect.setTextColor(getResources().getColorStateList(R.color.white));
                 TamSelect.setBackgroundTintList(getResources().getColorStateList(R.color.transparent));
                 TamSelect.setTextColor(getResources().getColorStateList(R.color.grey));
-                HindiSelect.setBackgroundTintList(getResources().getColorStateList(R.color.transparent));
-                HindiSelect.setTextColor(getResources().getColorStateList(R.color.grey));
                 language = "eng";
                 Intent loginIntent = new Intent(LanguageActivity.this, LoginActivity.class);
                 loginIntent.putExtra("language",language);
@@ -47,8 +44,7 @@ public class LanguageActivity extends AppCompatActivity {
             public void onClick(View v) {
                 TamSelect.setBackgroundTintList(getResources().getColorStateList(R.color.green));
                 TamSelect.setTextColor(getResources().getColorStateList(R.color.white));
-                HindiSelect.setBackgroundTintList(getResources().getColorStateList(R.color.transparent));
-                HindiSelect.setTextColor(getResources().getColorStateList(R.color.grey));
+                EngSelect.setTextColor(getResources().getColorStateList(R.color.grey));
                 EngSelect.setBackgroundTintList(getResources().getColorStateList(R.color.transparent));
                 language = "tam";
                 Intent loginIntent = new Intent(LanguageActivity.this, LoginActivity.class);
@@ -56,23 +52,6 @@ public class LanguageActivity extends AppCompatActivity {
                 loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(loginIntent);
 
-            }
-        });
-        HindiSelect.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-            @Override
-            public void onClick(View v) {
-                HindiSelect.setBackgroundTintList(getResources().getColorStateList(R.color.green));
-                HindiSelect.setTextColor(getResources().getColorStateList(R.color.white));
-                TamSelect.setBackgroundTintList(getResources().getColorStateList(R.color.transparent));
-                TamSelect.setTextColor(getResources().getColorStateList(R.color.grey));
-                EngSelect.setBackgroundTintList(getResources().getColorStateList(R.color.transparent));
-                EngSelect.setTextColor(getResources().getColorStateList(R.color.grey));
-                language = "hin";
-                Intent loginIntent = new Intent(LanguageActivity.this, LoginActivity.class);
-                loginIntent.putExtra("language",language);
-                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(loginIntent);
             }
         });
     }
