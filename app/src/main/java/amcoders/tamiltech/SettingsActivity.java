7f859617dce36c3b976eddf4c532066d8ac43d33
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-31 02:38:22 +0530

    Logout functionality added

--- app/src/main/java/amcoders/tamiltech/SettingsActivity.java
+++ app/src/main/java/amcoders/tamiltech/SettingsActivity.java
@@ -7,19 +7,24 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SettingsActivity extends AppCompatActivity {
     private Button logoutButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        mAuth= FirebaseAuth.getInstance();
 
         logoutButton = (Button) findViewById(R.id.logout_button);
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mAuth.signOut();
                 Intent dIntent = new Intent(SettingsActivity.this, DashboardActivity.class);
                 dIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(dIntent);
