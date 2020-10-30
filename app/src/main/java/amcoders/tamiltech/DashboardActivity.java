e12166d2ac0a015d8a46723f1c8445acf4f79878
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-30 22:24:59 +0530

    Created Transcript page UI

--- app/src/main/java/amcoders/tamiltech/DashboardActivity.java
+++ app/src/main/java/amcoders/tamiltech/DashboardActivity.java
@@ -1,14 +1,28 @@
 package amcoders.tamiltech;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
+    private LinearLayout transcriptLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        transcriptLayout = (LinearLayout) findViewById(R.id.transcript_llt);
+
+        transcriptLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tIntent = new Intent(DashboardActivity.this, TranscriptActivity.class);
+                startActivity(tIntent);
+            }
+        });
     }
 }
