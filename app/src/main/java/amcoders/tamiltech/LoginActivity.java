c3cb716ce45693c915086d8989905de5e0c01a9d
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-10-30 18:28:49 +0530

    Created RegisterActivity

--- app/src/main/java/amcoders/tamiltech/LoginActivity.java
+++ app/src/main/java/amcoders/tamiltech/LoginActivity.java
@@ -1,28 +1,78 @@
 package amcoders.tamiltech;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView text_view;
+    private TextView forgotLink;
+    private Button LoginButton;
+    private EditText emailET, passET;
+    private Button RegisterButton;
+    String email,password;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        text_view = findViewById(R.id.text_view);
+        forgotLink = (TextView) findViewById(R.id.forgot_link);
+        LoginButton = (Button) findViewById(R.id.login_button);
+        emailET = (EditText) findViewById(R.id.login_email_et);
+        passET = (EditText) findViewById(R.id.login_pass_et);
+        RegisterButton = (Button) findViewById(R.id.login_register_btn);
 
-        text_view.setOnClickListener(new View.OnClickListener() {
+        forgotLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailET.getText().toString();
+                if (TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(LoginActivity.this, "Please provide the email address", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    mAuth = FirebaseAuth.getInstance();
+                    mAuth.sendPasswordResetEmail(email)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else
+                                    {
+                                        String msg = task.getException().getMessage();
+                                        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+                }
+            }
+        });
+
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
             }
         });
+
+
     }
 }
